30d166c98a52381f2c320c857db39534b8d053e3
v-shzha1 <v-shzha1@JEFFREY-ZHAO>
2018-01-10 19:49:23 +0800

    add custom valiation function

--- App.Models/Sys/SysSampleModel.cs
+++ App.Models/Sys/SysSampleModel.cs
@@ -4,15 +4,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using App.Common;
 
 namespace App.Models.Sys
 {
+    [DataContract]
     public class SysSampleModel
     {
         [Display(Name = "ID")]
         public string Id { get; set; }
 
-
+        [NotNullExpression]
         [Display(Name = "名称")]
         public string Name { get; set; }
 
@@ -23,14 +26,15 @@ namespace App.Models.Sys
 
         [Display(Name = "生日")]
         public DateTime? Bir { get; set; }
-
+        [MaxLength(5)]
         [Display(Name = "照片")]
         public string Photo { get; set; }
 
-
+        [MinLength(5)]
         [Display(Name = "简介")]
         public string Note { get; set; }
 
+        [DateExpressionAttribute]
         [Display(Name = "创建时间")]
         public DateTime? CreateTime { get; set; }
 
